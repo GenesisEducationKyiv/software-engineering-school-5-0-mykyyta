bbd750897db3af6e8e88836e6b4abeee185d6c65
mykyyta <nikita.glushko@gmail.com>
2025-06-18 23:56:44 +0300

    start hw5 2

--- internal/weather/errors.go
+++ internal/weather/errors.go
@@ -1,1 +1,12 @@
 package weather
+
+import "errors"
+
+var ErrCityNotFound = errors.New("city not found")
+
+var (
+	ErrAPIUnavailable = errors.New("weather API unavailable")
+	ErrRateLimited    = errors.New("weather API rate limited")
+	ErrInvalidKey     = errors.New("invalid or missing API key")
+	ErrMalformedData  = errors.New("unexpected API response")
+)
